78b98bbde44ba0c89e2e151c38cd1a0b95226b92
schnatr <schnatr@gmail.com>
2023-12-28 15:07:55 +0700

    i swim

--- 01.py
+++ 01.py
@@ -4,10 +4,11 @@ from Starry import *
 
 
 def main():
+
   app = QApplication(sys.argv)
-  w1 = Simple_drawing_window1
-  w2 = Simple_drawing_window2
-  w3 = Simple_drawing_window3
+  w1 = Simple_drawing_window1()
+  w2 = Simple_drawing_window2()
+  w3 = Simple_drawing_window3()
 
   w1.show()
   w2.show()
