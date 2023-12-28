adb3a82b3eb655570bdffcad3012b84b0e8508a5
Amputchat Pramoj Na Ayudhya <152466932+amputchat0604@users.noreply.github.com>
2023-12-28 14:11:29 +0700

    new

--- 01.py
+++ 01.py
@@ -1,11 +1,14 @@
 from rabbit import *
+from christmasTree import *
 
 
 def main():
     app = QApplication(sys.argv)
 
     w = Simple_drawing_window1()
+    w2 = Simple_drawing_window2()
     w.show()
+    w2.show()
 
     return app.exec()
 
--- christmasTree.py
+++ christmasTree.py
@@ -3,7 +3,7 @@ from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 
-class Simple_drawing_window(QWidget):
+class Simple_drawing_window2(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Christmas Tree Drawing")
