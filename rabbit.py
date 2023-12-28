48fa7b291102dda156960b7c2c7160e8444c7a4b
schnatr <schnatr@gmail.com>
2023-12-28 13:59:47 +0700

    rabbit

---
+++ rabbit.py
@@ -0,0 +1,26 @@
+import sys
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+
+class Simple_drawing_window(QWidget):
+    def __init__(self):
+        QWidget.__init__(self,None)
+        self.setWindowTitle("Simple GitHub Drawing")
+        self.rabbit = QPixmap("images/rabbit.png")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p = QPainter()
+        p.begin(self)
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(255,255,255))  
+        p.drawEllipse(250, 150, 100, 150) 
+        p.drawEllipse(280, 180, 20, 40)  
+        p.drawEllipse(300, 180, 20, 40)  
+        p.drawEllipse(270, 250, 20, 10)  
+        p.drawEllipse(310, 250, 20, 10)  
+
+        p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)  
+
+        p.end()
