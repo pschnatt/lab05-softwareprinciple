b63efb3fde42b81dca4387048975f7b79a52bf89
Amputchat Pramoj Na Ayudhya <152466932+amputchat0604@users.noreply.github.com>
2023-12-28 15:10:34 +0700

    new

--- 03.py
+++ 03.py
@@ -1,3 +1,5 @@
+import turtle as t
+
 class Hanoi(object):
     def __init__(self, n=3, start="A", workspace="B", destination="C"):
         self.startp = Pole(start, 0, 0)
@@ -23,6 +25,58 @@ class Hanoi(object):
 
     def solve(self):
         self.move_tower(3, self.startp, self.destinationp, self.workspacep)
+
+class Disk(object):
+    def __init__(self, name="", xpos=0, ypos=0, height=20, width=40):
+        self.dname = name
+        self.dxpos = xpos
+        self.dypos = ypos
+        self.dheight = height
+        self.dwidth = width
+
+    def showdisk(self):
+        t.pensize(2)
+        t.speed(0)
+        t.pendown()
+        t.fillcolor("grey")
+        t.begin_fill()
+
+        for _ in range(2):
+            t.fd(200)
+            t.left(90)
+            t.fd(30)
+            t.left(90)
+        t.fd(100)
+
+        t.end_fill()
+
+    def newpos(self, x, y):
+        t.penup()
+        t.goto(x, y)
+        t.pendown()
+        self.dxpos, self.dypos = x, y
+
+    def cleardisk(self):
+        # Precondition: Turtle must face east
+        t.clear()
+        t.setheading(0)
+
+        # Move to the current position of the disk
+        t.penup()
+        t.goto(self.dxpos, self.dypos)
+        t.pendown()
+
+        # Clear the disk
+        t.fillcolor("grey")
+        t.begin_fill()
+        for _ in range(2):
+            t.fd(200)
+            t.left(90)
+            t.fd(30)
+            t.left(90)
+        t.fd(100)
+
+        t.end_fill()
 
 h = Hanoi()
 h.solve()
