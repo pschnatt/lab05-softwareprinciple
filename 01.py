e03f01177d69d59604a724336076d7e205758423
schnatr <schnatr@gmail.com>
2023-12-28 14:07:02 +0700

    rabbit

--- 01.py
+++ 01.py
@@ -1,1 +1,13 @@
-print("hello world")vfvfvrvf
+from rabbit import *
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    w = Simple_drawing_window1()
+    w.show()
+
+    return app.exec()
+
+if __name__ == "__main__":
+    sys.exit(main())
