9233f396057a9f9d952e55d7e5e13d0ff3f3d529
shinyyxxx <puriwat888@gmail.com>
2023-12-28 15:45:21 +0700

    f th rip

--- 03.py
+++ 03.py
@@ -1,19 +1,100 @@
 import turtle as t
 
-class Hanoi(object):
+class Disk:
+    def __init__(self, name, xpos, ypos, height, width):
+        self.dname = name
+        self.dxpos = xpos
+        self.dypos = ypos
+        self.dheight = height
+        self.dwidth = width
+        self.t = t.Turtle()
+        self.t.hideturtle()
+        self.t.speed('fastest')
+
+    def draw_disk(self):
+        self.t.penup()  # Lift the pen before moving
+        self.t.goto(self.dxpos, self.dypos - self.dheight / 2)  # Adjust for the disk's height
+        self.t.pendown()
+        self.t.begin_fill()
+        for _ in range(2):
+            self.t.forward(self.dwidth / 2)
+            self.t.left(90)
+            self.t.forward(self.dheight)
+            self.t.left(90)
+            self.t.forward(self.dwidth)
+            self.t.left(90)
+            self.t.forward(self.dheight)
+            self.t.left(90)
+            self.t.forward(self.dwidth / 2)
+        self.t.end_fill()
+        self.t.penup()
+
+    def newpos(self, xpos, ypos):
+        self.dxpos = xpos
+        self.dypos = ypos
+        self.draw_disk()
+
+    def cleardisk(self):
+        self.t.reset()
+        self.t.hideturtle()
+
+class Pole:
+    def __init__(self, name, xpos, ypos, thick, length):
+        self.pname = name
+        self.stack = []
+        self.pxpos = xpos
+        self.pypos = ypos
+        self.pthick = thick
+        self.plength = length
+        self.t = t.Turtle()
+        self.t.hideturtle()
+        self.t.speed('fastest')
+
+    def showpole(self):
+        self.t.penup()
+        self.t.goto(self.pxpos, self.pypos)
+        self.t.pendown()
+        self.t.forward(self.pthick / 2)
+        self.t.left(90)
+        self.t.forward(self.plength)
+        self.t.left(90)
+        self.t.forward(self.pthick)
+        self.t.left(90)
+        self.t.forward(self.plength)
+        self.t.left(90)
+        self.t.forward(self.pthick / 2)
+        self.t.penup()
+
+    def pushdisk(self, disk):
+        disk.cleardisk()  # Clear the disk before moving it
+        ypos = self.pypos + len(self.stack) * disk.dheight
+        disk.newpos(self.pxpos, ypos)
+        self.stack.append(disk)
+
+    def popdisk(self):
+        if self.stack:
+            disk = self.stack.pop()
+            disk.cleardisk()  # Clear the disk before moving it
+            return disk
+        return None
+
+class Hanoi:
     def __init__(self, n=3, start="A", workspace="B", destination="C"):
-        self.startp = Pole(start, 0, 0)
-        self.workspacep = Pole(workspace, 150, 0)
-        self.destinationp = Pole(destination, 300, 0)
+        self.startp = Pole(start, -150, -100, 10, 100)
+        self.workspacep = Pole(workspace, 0, -100, 10, 100)
+        self.destinationp = Pole(destination, 150, -100, 10, 100)
         self.startp.showpole()
         self.workspacep.showpole()
         self.destinationp.showpole()
         for i in range(n):
-            self.startp.pushdisk(Disk("d"+str(i), 0, i*150, 20, (n-i)*30))
+            disk = Disk("d"+str(i+1), self.startp.pxpos, self.startp.pypos + i*20, 20, (n-i)*30 + 10)
+            self.startp.pushdisk(disk)
+            disk.draw_disk()
 
     def move_disk(self, start, destination):
         disk = start.popdisk()
-        destination.pushdisk(disk)
+        if disk:
+            destination.pushdisk(disk)
 
     def move_tower(self, n, s, d, w):
         if n == 1:
@@ -26,95 +107,14 @@ class Hanoi(object):
     def solve(self):
         self.move_tower(3, self.startp, self.destinationp, self.workspacep)
 
-class Pole(object):
-  def __init__(self,name="",xpos=0,ypos=0,thick=10,length=100):
-    self.pname = name
-    self.stack = []
-    self.toppos = 0
-    self.pxpos = xpos
-    self.pypos = ypos
-    self.pthick = thick
-    self.plength = length
+# Set up the screen
+t.setup(600, 600)
+t.title("Tower of Hanoi")
+t.bgcolor("white")
 
-  def showpole(self):
-    t.penup()
-    t.goto(self.pxpos,self.pypos)
-    t.pendown()
-    t.fd(self.pthick / 2)
-    t.lt(90)
-    t.fd(self.plength)
-    t.lt(90)
-    t.fd(self.pthick)
-    t.lt(90)
-    t.fd(self.plength)
-    t.lt(90)
-    t.fd(self.pthick / 2)
-    
-  def pushdisk(self, disk):
-    if (disk.dheight * self.toppos >= self.plength):
-      return
-    disk.cleardisk()
-    disk.newpos(self.pxpos,self.pypos + disk.dheight * self.toppos)
-    disk.showdisk()
-
-  def popdisk(self):
-    if (self.toppos > 0):
-      disk = self.stack.pop()
-      position = disk.dheight * (len(self.toppos) - 1)
-      self.toppos -= 1
-      disk.cleardisk()
-      disk.newpos(self.pxpos,self.pypos + self.plength + 20)
-      disk.showdisk()
-
-
-class Disk(object):
-    def __init__(self, name="", xpos=0, ypos=0, height=20, width=40):
-        self.dname = name
-        self.dxpos = xpos
-        self.dypos = ypos
-        self.dheight = height
-        self.dwidth = width
-
-    def showdisk(self):
-        t.pensize(2)
-        t.speed(0)
-        t.pendown()
-        t.fillcolor("grey")
-        t.begin_fill()
-
-        for _ in range(2):
-            t.fd(self.dwidth)
-            t.left(90)
-            t.fd(self.dheight)
-            t.left(90)
-        t.fd(100)
-
-        t.end_fill()
-
-    def newpos(self, x, y):
-        t.penup()
-        t.goto(x, y)
-        t.pendown()
-        self.dxpos, self.dypos = x, y
-
-    def cleardisk(self):
-        t.clear()
-        t.setheading(0)
-
-        t.penup()
-        t.goto(self.dxpos, self.dypos)
-        t.pendown()
-
-        t.fillcolor("grey")
-        t.begin_fill()
-        for _ in range(2):
-            t.fd(self.dwidth)
-            t.left(90)
-            t.fd(self.dheight)
-            t.left(90)
-        t.fd(100)
-
-        t.end_fill()
-
+# Create and solve the puzzle
 h = Hanoi()
 h.solve()
+
+# Finish the program
+t.done()
